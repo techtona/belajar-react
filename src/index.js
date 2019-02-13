9e230c105970f0f445ed1fb51462af5fb7d54465
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-02-13 08:40:31 +0700

    pertemuan 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function getName(mahasiswa){
+    return 'Nama : ' + mahasiswa.nama + ' NIM :'+mahasiswa.nim;
+}
+
+function selamatDatang(mahasiswa){
+    if(mahasiswa.angkatan < 2019){
+        return "Selamat datang Maba " + mahasiswa.nama;
+    }else{
+        return ".....";
+    }
+}
+
+const mahasiswi = {
+    nama : 'Lorem Ipsum',
+    nim : '123456',
+    angkatan : 2018
+}
+
+const element = (<h1>
+    Mahasiswa {selamatDatang(mahasiswi)}
+</h1>);
+
+// const nama = 'Lorem Ipsum';
+// const element = <h1>Nama Saya adalah {nama}</h1>
+
+ReactDOM.render(element, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
