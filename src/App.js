3c14caf026a5f589d7de6779942abe0930cf8308
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-03-26 10:38:56 +0700

    setState and fetch API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  Product from './Product'
+import Product from './Product'
+
+// import Banner from './Banner/Banner'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Product judul="Powerbank" harga="10.000"/>
-        <Product judul="Powerbank" harga="10.000"/>
-        <Product judul="Powerbank" harga="10.000"/>
-        <Product judul="Powerbank" harga="10.000"/>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://my-json-server.typicode.com/techtona/product_api2/products')
+            .then(response => response.json())
+            .then((data) => {
+                this.setState({products: data})
+            })
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    {
+                        this.state.products.map((data, key) =>
+                            <div key={data.id} className="col-md-3">
+                                <Product className="column" judul={data.judul} harga={data.harga}/>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default App;
--- src/Product.js
+++ src/Product.js
@@ -6,11 +6,10 @@ import logo from "./logo.svg"
 class Product extends React.Component{
     render(){
         return(
-            <div>
+            <div className={this.props.className}>
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>{this.props.judul}</h1>
-                <h3>Rp. {this.props.harga}
-            </h3>
+                <h3>{this.props.judul}</h3>
+                <p className="orange"><b>{this.props.harga}</b></p>
             </div>);
     }
 }
