bf3459fe79545aa64ef9234f0d7a8dda43a3f147
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-03-27 10:38:55 +0700

    tabel mahasiswa

--- src/App.js
+++ src/App.js
@@ -1,40 +1,90 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Product from './Product'
-
-// import Banner from './Banner/Banner'
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            products: []
+            mahasiswa: []
         }
     }
 
-    componentDidMount() {
-        fetch('https://my-json-server.typicode.com/techtona/product_api2/products')
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({products: data})
-            })
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        let nim = this.refs.nim.value;
+        let nama = this.refs.nama.value;
+        let gender = this.refs.gender.value;
+
+        this.state.mahasiswa.push({nim, nama, gender});
+        this.setState({mahasiswa: this.state.mahasiswa});
+        // console.log(this.state.mahasiswa)
+        this.refs.form.reset();
+        this.refs.nim.focus();
     }
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    {
-                        this.state.products.map((data, key) =>
-                            <div key={data.id} className="col-md-3">
-                                <Product className="column" judul={data.judul} harga={data.harga}/>
-                            </div>
-                        )
-                    }
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card">
+                        <div className="card-header">
+                            Tambahkan Mahasiswa
+                        </div>
+                        <div className="card-body">
+                            <form ref="form" onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label>NIM</label>
+                                    <input type="text" ref="nim" className="form-control" placeholder="NIM"/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Nama</label>
+                                    <input type="text" ref="nama" className="form-control" placeholder="Nama"/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Gender</label>
+                                    <select name="" ref="gender" className="form-control">
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                    </select>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Tambah</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">
+                            Tabel Mahasiswa
+                        </div>
+                        <table id="tabel-mahasiswa" className="table-hover table table-bordered">
+                            <thead>
+                            <tr>
+                                <th>NIM</th>
+                                <th>NAMA</th>
+                                <th>GENDER</th>
+                                <th>OPTION</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.mahasiswa.map((data, key) =>{
+                                    return (
+                                        <tr key={key}>
+                                            <td>{data.nama}</td>
+                                            <td>{data.nim}</td>
+                                            <td>{data.gender}</td>
+                                            <td>
+                                                <input onClick="" type="button" value="Hapus" className="btn btn-sm btn-danger"/>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-
         );
     }
 }
